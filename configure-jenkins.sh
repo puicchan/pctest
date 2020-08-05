a035e2ae769dc263385ff0e0cd61726974d8f89f
puicchan <puichan@Microsoft.com>
2020-08-05 16:29:09 -0700

    Update configure-jenkins.sh

--- configure-jenkins.sh
+++ configure-jenkins.sh
@@ -6,10 +6,12 @@ sudo apt-get upgrade
 sudo apt install openjdk-8-jdk -y
 
 # Install Ansible
-wget -q -O - https://pkg.jenkins.io/debian/jenkins-ci.org.key | sudo apt-key add -
+wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo apt-key add -
 
 sudo sh -c 'echo deb https://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
 
-sudo apt-get update && sudo apt-get install jenkins -y
+sudo apt-get update 
+
+sudo apt-get install jenkins -y
 
 sudo service jenkins restart
