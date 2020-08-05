6332919ad24115b9bda58b9a34106494b9101409
puicchan <puichan@Microsoft.com>
2020-08-05 15:36:22 -0700

    Update configure-jenkins.sh

--- configure-jenkins.sh
+++ configure-jenkins.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
+
 # Install OpenJDK
 sudo apt install openjdk-8-jdk -y
+
 # Install Ansible
 wget -q -O - https://pkg.jenkins.io/debian/jenkins-ci.org.key | sudo apt-key add -
+
 sudo sh -c 'echo deb https://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
+
 sudo apt-get update && sudo apt-get install jenkins -y
+
 sudo service jenkins restart
